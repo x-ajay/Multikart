3bd1572b38d18d0a06d288d065e997aef6e8557f
NikitaFunde <nikita.funde15@gmail.com>
2024-02-08 21:18:39 +0530

    Added 4_img & prod_details

--- src/app/products/four-image/four-image.component.ts
+++ src/app/products/four-image/four-image.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./four-image.component.css']
 })
 export class FourImageComponent {
+  // images
+  public img1="../../../assets/Images/pro3/001.fb2774aa.jpg";
+  public img2="../../../assets/Images/pro3/002.56d02228.jpg";
+  public img3="../../../assets/Images/pro3/003.b59d96eb.jpg";
+  public img4="../../../assets/Images/pro3/004.ee6aaa49.jpg";
 
+  //links
+  public facebook="https://www.facebook.com";
+  public google="https://plus.google.com";
+  public twitter="https://twitter.com";
+  public instagram="https://www.instagram.com";
+  public rss="https://rss.com";
 }
